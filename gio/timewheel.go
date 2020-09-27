2381e69a7e6a0ba82b7133bc91c4b3bf1bd09c19
Togou98 <togou98@gmail.com>
2020-09-27 19:42:25 +0800

    simple timewheel implement

---
+++ gio/timewheel.go
@@ -0,0 +1,26 @@
+
+import (
+	"container/list"
+	"time"
+)
+
+type TimeWheel struct {
+	interval time.Duration
+	tick     <-chan time.Time
+	curPos   int
+	maxPos   int
+	callback func(interface{})
+	lst []*list.List
+}
+func(t *TimeWheel)start(){
+	t.tick = time.Tick(t.interval)
+	for _ := range t.tick{
+		//1s
+	}
+}
+type Task struct {
+	delay time.Duration
+	circle int
+}
+
+
