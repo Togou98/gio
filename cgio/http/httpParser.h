f148d266f58b699747c54e191b15f244fffb6826
Togou <togou98@gmail.com>
2020-10-27 00:33:50 +0800

    master

--- cgio/http/httpParser.h
+++ cgio/http/httpParser.h
@@ -1,10 +1,13 @@
 #include <string>
 #include <cstring>
+#include <map>
+#include <functional>
+#include <sstream>
+#include <unistd.h>
 using namespace std;
-
-class HttpStruct;
 class Response;
 class Request;
+class HttpStruct;
 
 class Http
 {
@@ -13,32 +16,50 @@ public:
     ~Http();
     string rawStr;
     Http &Parse(string);
+    Http& parseHttp();
     Request *Req;
     Response *Res;
-    void parseHttp();
-
-
+    void GET();
+    void POST();
+    map<string,void(*)(const Request*,Response*)> phfMap;
+    string doResponse();
 private:
 };
 
 struct Request
 {
-    Request(string,string);
+    Request(string, string);
     void parseLine();
     void parseOtherLine(string);
     void parseHead();
     void parseBody();
     void parseKeyValue(string);
+    void specialFilter();
     string rawHeader;
     string rawBody;
-///////////////////
+    ///////////////////
     string Method;
     string Path;
     string Version;
+    map<string, string> Header;
+    bool Keep_Alive;
+    int Content_Length = 0;
+    bool Done;
+    string Host;
 };
 struct Response
-{
-    Response(string);
-};
+{   Response();
+    Response(Request *);
+    string Page404();
+    string Page500();
+    void WriteString(string);
+    string String();
+    map<string, string> Header;
+    int ContentLength()const;
+    bool Keep_Alive;
+    int Code;
+    string Message;
+private:
+    string PayLoad;
 
-string str = "GET /Post HTTP/1.1\r\nHost: 100.100.1\r\nContent-Length: 184\r\n\r\nDATATEMPLATE";
+};
